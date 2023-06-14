3e6810955384d46767c7e1e72dae139b821aaffd
Chris Millar <cmillar@adobe.com>
2023-06-14 17:47:02 -0600

    minor cache policy polish

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const MILO_HOSTNAME = 'main--milo--adobecom.hlx.live';
 const ORIGIN_HOSTNAME = 'main--cmillar--auniverseaway.hlx.live';
 
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     try {
       const url = new URL(request.url);
       url.hostname = url.pathname.startsWith('/libs')
         ? MILO_HOSTNAME
         : ORIGIN_HOSTNAME;
       const req = new Request(url, request);
+      const opts = { cf: {} };
       req.headers.set('x-forwarded-host', req.headers.get('host'));
       req.headers.set('x-byo-cdn-type', 'cloudflare');
-      let resp = await fetch(req, {
-        cf: { cacheEverything: false },
-      });
+
+      // Enable caching by uncommenting the next two lines.
+      // req.headers.set('x-push-invalidation', 'enabled');
+      // opts.cf.cacheEverything = true;
+
+      let resp = await fetch(req, opts);
       resp = new Response(resp.body, resp);
       resp.headers.delete('age');
       resp.headers.delete('x-robots-tag');
